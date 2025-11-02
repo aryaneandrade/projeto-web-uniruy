b66e4ba0cc24672d9e6de925bbbd0b56b301c25b
Aryane <aryaneands@gmail.com>
2025-11-02 19:16:03 -0300

    adicionando arquivo de conexão

---
+++ conexao.php
@@ -0,0 +1,21 @@
+<?php
+
+// Conexão com o banco de dados usando PDO
+$host = 'localhost';
+$db   = 'black_friday';
+$user = 'root';
+$pass = 'Admin@123';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}   
+?>
